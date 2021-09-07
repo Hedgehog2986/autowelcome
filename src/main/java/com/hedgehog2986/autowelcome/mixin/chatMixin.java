5e5ac0f8cc84908a913c994d5a047ac9f4d487a0
Hedgehog2986 <70155771+Hedgehog2986@users.noreply.github.com>
2021-09-07 21:41:36 +0100

    Update chatMixin.java

--- src/main/java/com/hedgehog2986/autowelcome/mixin/chatMixin.java
+++ src/main/java/com/hedgehog2986/autowelcome/mixin/chatMixin.java
@@ -1,4 +1,5 @@
 package com.hedgehog2986.autowelcome.mixin;
+
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.text.Text;
@@ -9,7 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.nio.file.Path;
 import java.util.Scanner;
 
 @Mixin(ChatHud.class)
@@ -17,7 +17,7 @@ public abstract class chatMixin {
     @Inject(method = "addMessage(Lnet/minecraft/text/Text;)V", at = @At("HEAD"), cancellable = true)
     private void readMessage(Text message, CallbackInfo ci) {
         assert message.getString().trim().length() >= 12;
-        var messageString = message.getString().trim().substring(0, 11);
+        var messageString = message.getString().trim();
         File messageFile = new File("welcomemsg.txt");
         var welcomeMessage = "";
         try {
@@ -25,9 +25,10 @@ public abstract class chatMixin {
         } catch (FileNotFoundException ignored) {
             welcomeMessage = "welcome";
         }
-        if (messageString.equals("[ WELCOME ]")) {
+        if (messageString.startsWith("[ WELCOME ]")) {
+            var username = messageString.split(" ")[4];
             assert MinecraftClient.getInstance().player != null;
-            MinecraftClient.getInstance().player.sendChatMessage(welcomeMessage);
+            MinecraftClient.getInstance().player.sendChatMessage(welcomeMessage.replace("[user]",username));
         }
     }
 }
